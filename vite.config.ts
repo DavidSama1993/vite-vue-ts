8f4756d6e5f1437138e020a47cd9ae3df0964b74
DavidSama1993 <18210222071@163.com>
2021-06-25 00:42:01 +0800

    修改一些项目基础配置，有许多需要加注释的地方，慢慢消化~

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+/**
+ * 对于使用 "export = Mod" 方式导出的模块
+ * 必须使用 "import * as Mod from '<ModPath>'" 方式引入
+ */
+import * as path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()]
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src')
+    }
+  }
 })
